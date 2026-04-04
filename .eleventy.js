8d4ca5b92d00bcda8799f895864cf18f96a77bed
nlowthorpe <nick.lowthorpe@googlemail.com>
2026-04-04 21:59:03 +0100

    fixing path structurs

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
-module.exports = function(eleventyConfig) {
-  // Pass through the assets folder for CSS and Images to be accessible in output
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
 
-  eleventyConfig.addCollection("posts", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("posts/*.md");
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("posts/*.md").reverse();
   });
+
+  return {
+    pathPrefix: "/blog/"
+  };
 };
